ddecc56f77d1288622ff0c9f443198765674e898
Beckett Loose <beckettloose@me.com>
2019-02-24 12:32:00 -0500

    Updated Scripts

--- scripts/dpm install.sh
+++ scripts/dpm install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+eval $(parse_yaml packages.yml "package_")
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
@@ -38,7 +53,13 @@ ERR="[ERROR] "
 echo "${PFX} Installing Package ${PACKAGE}"
 if [ -z "$PACKAGE" ]
 then
-      echo "No Package Name Given! Try Again."
+      echo "${PFX} ${ERR} No Package Name Given! Try Again."
 else
-      
+	echo "Getting Install URL"
+	InstallURL=${packages_${PACKAGE}_install-url}
+	cd temp/
+	mkdir ${PACKAGE}
+	cd ${PACKAGE}
+	wget  ${InstallURL} -q --show-progress
+	echo "${PFX} Downloaded ${packages_${package}_normal-name}"
 fi
