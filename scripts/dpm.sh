70c007044960a6aae1c7c63e621bf88dfb600154
BlockBusterBPL <beckettloose@me.com>
2019-02-24 14:54:05 -0500

    Added Some New Scripts And Fixed The Install Command. TODO: Config For Minecraft World Config And Installer.

---
+++ scripts/dpm.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+. parse_yaml.sh
+eval $(parse_yaml packages.yml "package_")
+#eval $(parse_yaml configuration.yml "config_")
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -p|--package)
+    PACKAGE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -m|--mode)
+    MODE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -r|--reset)
+    RESET="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -u|--update)
+    UPDATE=YES
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+PFX="[DPM]: "
+WARN="[WARNING] "
+ERR="[ERROR] "
+if [ "$MODE" = "install" ]
+if [ -z "$PACKAGE" ]
+then
+      echo "${PFX}${ERR}No Package Name Given! Try Again."
+else
+	echo "${PFX}Getting Install URL"
+	echo "${PFX}Grabbed Install URL For $PACKAGE"
+	rmdir ${PACKAGE}
+	mkdir ${PACKAGE}
+	cd ${PACKAGE}
+	wget $package_testfile_installurl --show-progress
+	echo "${PFX}Downloaded $package_testfile_normalname"
+fi
+else
+	
+	if []
+
+	else
+	
+	fi
+	
+fi
