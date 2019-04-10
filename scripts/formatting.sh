96836e66aa543ee9561a1bde179cbd79439115fa
BlockBusterBPL <beckettloose@gmail.com>
2019-04-10 12:33:51 -0400

    Update formatting.sh

--- scripts/formatting.sh
+++ scripts/formatting.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 # Not Done Yet!
 # Formatting Utilities By BlockBusterBPL
 # Use these variables in place of complicated ANSI formatting codes.
 # Usage: run this script from your .bash_profile to set these environment variables
 # Echo commands must have '-e' argument for this to work. e.g. 'echo -e "${color_red}This Text Is Red!"'
-
-#!!BEGIN COLOR SECTION!!#
+# Remember Dialog Command from invisible island
+#BEGIN COLOR SECTION#
 export color_default="\e[39m"
 export color_black="\e[30m"
 export color_red="\e[31m"
@@ -16,3 +17,10 @@ export color_cyan
 export color_lightgray
 export color_darkgray
 export color_lightred
+export color_lightgreen
+export color_lightyellow
+export color_lightblue
+export color_lightmagenta
+export color_lightcyan
+export color_white
+#END COLOR SECTION#
