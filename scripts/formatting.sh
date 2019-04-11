75e047bfb799872e59fc283daf203838f449e5d4
BlockBusterBPL <beckettloose@gmail.com>
2019-04-11 08:42:42 -0400

    Update formatting.sh

--- scripts/formatting.sh
+++ scripts/formatting.sh
@@ -5,6 +5,24 @@
 # Usage: run this script from your .bash_profile to set these environment variables
 # Echo commands must have '-e' argument for this to work. e.g. 'echo -e "${color_red}This Text Is Red!"'
 # Remember Dialog Command from invisible island
+
+#BEGIN FONT STYLE SECTION#
+export font_bold
+export font_dim
+export font_under
+export font_blink
+export font_invert
+export font_hide
+#FONT STYLE RESET
+export font_reset
+export font_bold_end
+export font_dim_end
+export font_under_end
+export font_blink_end
+export font_invert_end
+export font_hide_end
+#END FONT STYLE SECTION
+
 #BEGIN COLOR SECTION#
 export color_default="\e[39m"
 export color_black="\e[30m"
