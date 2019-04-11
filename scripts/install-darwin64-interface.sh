57beaa38c6eacbe5e2c73d7fd40ab5530a30c211
BlockBusterBPL <beckettloose@me.com>
2019-04-11 18:50:59 -0400

    Worked On Graphical Installation Scripts For The Software

--- scripts/install-darwin64-interface.sh
+++ scripts/install-darwin64-interface.sh
@@ -5,15 +5,28 @@ echo "starting installer"
 : ${DIALOG_EXTRA=3}
 : ${DIALOG_ITEM_HELP=4}
 : ${DIALOG_ESC=255}
-tmp_file=$(tempfile 2>/dev/null) || tmp_file=/tmp/test$$
-trap "rm -f $tmp_file" 0 1 2 5 15
-
 echo "Checking Required Libraries..."
 if [ colortest ] ; then
-
+:
 else
 echo "One or more libraries were not found! Make sure they are installed before you run this script!"
 wait 3
 exit 1
 fi
-dialog --checklist "Select Options To Install:" 10 40 4
+dialog --checklist "Select Options To Install:" 17 66 4 \
+1 "DPM Core (Required)" 1 \
+2 "GamePack Development Kit" 0 \
+3 "Preinstall Package List" 0 \
+4 "stuff" 0
+
+clear
+
+dialog --gauge "Installation Progress" 17 66 0
+
+clear
+
+dialog --msgbox "An Error Has Occured! \n The installation will now exit..." 17 66
+wait
+clear
+echo "An Error Occured! Code: 2: Installation Error Occured... No Further Information"
+exit 1
