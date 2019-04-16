9cdad36e4917123415f4467a04d3cd5361f0acde
BlockBusterBPL <beckettloose@gmail.com>
2019-04-16 12:33:55 -0400

    Update dpm-graphical.sh

--- scripts/dpm-graphical.sh
+++ scripts/dpm-graphical.sh
@@ -6,11 +6,27 @@
 ###################################
 version="0.1"
 clear
+mkfifo /tmp/dpm/dialogchoice
+
 dialog --title "DPM" --menu "Choose An Option" 44 66 4 \
 1 "Select A World" \
 2 "Change Global Settings" \
 3 "Credits and Information" \
-4 "Update Package List" 
+4 "Update Package List" 2> /tmp/dpm/dialogchoice
+
+dialogchoice="$( cat /tmp/dpm/dialogchoice )"
+
+case $dialogchoice in 
+  "Select A World")
+    world_select
+      ;;
+  "Change Global Settings")
+    global_settings
+      ;;
+  *)
+    clear
+    echo "An Error Occured With DPM! Error: invalid menu choice"
+    exit 1
 
 world_select(){
 yq read "./worlds.yml" worldnames
