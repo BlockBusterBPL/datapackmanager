46f5bf71938caa5fb7be674026e2e1fef1c479a9
BlockBusterBPL <beckettloose@gmail.com>
2019-04-17 11:24:09 -0400

    Update datapackmaker.sh

--- scripts/datapackmaker.sh
+++ scripts/datapackmaker.sh
@@ -1,1 +1,22 @@
 # DataPackMaker v0.1 by: BlockBusterBPL
+# Define Functions
+log(){
+:
+}
+log_init(){ # Initialize Logger
+  mkfifo dpc-log # Create Named Pipe For Log
+  
+}
+log_teardown(){ # Tear Down and Close The Logger
+:
+}
+handle_changeover(){ # Handles A Seamless Switch From DPM to DPC
+:
+}
+handle_reverse_changeover(){ # Handles A Seamless Switch From DPC to DPM
+:
+}
+clear
+main_menu{
+dialog 
+}
