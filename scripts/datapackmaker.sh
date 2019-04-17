07606058f03f9cb0614e5f6729379d45b126b8e4
BlockBusterBPL <beckettloose@gmail.com>
2019-04-17 11:32:05 -0400

    Update datapackmaker.sh

--- scripts/datapackmaker.sh
+++ scripts/datapackmaker.sh
@@ -1,7 +1,12 @@
 # DataPackMaker v0.1 by: BlockBusterBPL
 # Define Functions
+source errorhandler-dpc.sh
 log(){
 :
+}
+program_exit(){
+  code="$1"
+  errorhandler-dpc $code
 }
 log_init(){ # Initialize Logger
   mkfifo dpc-log # Create Named Pipe For Log
@@ -16,7 +21,11 @@ handle_changeover(){ # Handles A Seamless Switch From DPM to DPC
 handle_reverse_changeover(){ # Handles A Seamless Switch From DPC to DPM
 :
 }
-clear
 main_menu{
-dialog 
+dialog
 }
+datapack_createnew(){
+  log "Creating New DataPack"
+  dialog --form
+}
+clear
