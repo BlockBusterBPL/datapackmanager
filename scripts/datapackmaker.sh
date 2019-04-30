d8a7a0937d20acaabb9b23d2aa8dd96f9afbc077
BlockBusterBPL <beckettloose@gmail.com>
2019-04-30 09:17:27 -0400

    Update datapackmaker.sh

--- scripts/datapackmaker.sh
+++ scripts/datapackmaker.sh
@@ -5,22 +5,22 @@ log(){
 :
 }
 mkcd(){ # Make And Enter Directory
-mkdir "${1}"
-cd "${1}"
+mkdir "$1"
+cd "$1"
 }
 program_exit(){
   errorhandler "dpc" $1
   exit 1
 }
 log_init(){ # Initialize Logger
-  mkfifo dpc-log # Create Named Pipe For Log
-  
+  mkfifo /tmp/dpc-log # Create Named Pipe For Log
 }
 log_teardown(){ # Tear Down and Close The Logger
 :
 }
 handle_changeover(){ # Handles A Seamless Switch From DPM to DPC
-mkfifo dpc-formout
+mkfifo /tmp/dpc-formout
+form=/tmp/dpc-formout
 }
 handle_reverse_changeover(){ # Handles A Seamless Switch From DPC to DPM
 :
@@ -66,16 +66,16 @@ datapack_createnew(){
   "Namespace" "" \
   "Author" "" \
   "Min. Version" "" \
-  "Max. Version" "" 2> dpc-formout
+  "Max. Version" "" 2> $form
   dpc_name_temp="$(cat dpc-formout | sed '1q;d')"
 	dpc_name="${dpc_name_temp#*Name }"
-  dpc_namespace_temp="$(cat dpc-formout | sed '2q;d')"
+  dpc_namespace_temp="$(cat $form | sed '2q;d')"
 	dpc_namespace="${dpc_namespace_temp#*Namespace }"
-  dpc_author_temp="$(cat dpc-formout | sed '3q;d')"
+  dpc_author_temp="$(cat $form | sed '3q;d')"
 	dpc_author="${dpc_author_temp#*Author }"
-  dpc_min_ver_temp="$(cat dpc-formout | sed '4q;d')"
+  dpc_min_ver_temp="$(cat $form | sed '4q;d')"
 	dpc_min_ver="${dpc_min_ver_temp#*Version }"
-  dpc_max_ver_temp="$(cat dpc-formout | sed '5q;d')"
+  dpc_max_ver_temp="$(cat $form | sed '5q;d')"
 	dpc_max_ver="${dpc_max_ver_temp#*Version }"
   cd datapacks
   mkdir "${dpc_name}"
