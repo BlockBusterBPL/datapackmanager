1ab678a16c6d26b82255cfb71657b483d31a09ab
BlockBusterBPL <beckettloose@gmail.com>
2019-05-07 12:27:59 -0400

    Update addrepo.sh

--- scripts/addrepo.sh
+++ scripts/addrepo.sh
@@ -1,1 +1,15 @@
-
+clone_repo() {
+  # Change to repos directory
+  git clone $1 # In Dialog
+  yq write repos.yml installed.[+] "$1"
+}
+remove_repo() {
+  # Change to repos directory
+  rm -rf "$1"
+  yq remove repos.yml installed."$1"
+  yq remove repos.yml refrenced."$1"
+}
+add_repo() {
+  yq write repos.yml refrenced.[+] "$1"
+  clone_repo "$1"
+}
