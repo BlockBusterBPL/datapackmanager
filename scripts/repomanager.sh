88c8ac433e097be3585f79e86ca3d556ae17c3c4
BlockBusterBPL <beckettloose@gmail.com>
2019-05-10 11:06:36 -0400

    Update repomanager.sh

--- scripts/repomanager.sh
+++ scripts/repomanager.sh
@@ -13,3 +13,19 @@ repo_add() {
   yq write repos.yml refrenced.[+] "$1"
   clone_repo "$1"
 }
+manage_repos(){
+  dialog --buildlist "Manage Repositories" 60 40 4 \
+  default "Default Repository" on \
+  test1 "Test Extra Repo On" on \
+  test2 "Test Extra Repo Off" off 2> $TEMP_MANAGER
+  yq write repos.yml installed null
+  for $1 in $(cat $TEMP_FILE)
+  do
+    reponame="$(yq read repos.yml refrenced."$1".repourl)"
+    git clone "reponame" $reponame
+    yq write repos.yml installed.[+] $1
+  done
+}
+repomanager(){
+  :
+}
