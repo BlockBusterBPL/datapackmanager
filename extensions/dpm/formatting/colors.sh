3ed8fc66ad83c5c396400cf81754f3c939d75a4c
BlockBusterBPL <beckettloose@me.com>
2019-09-25 09:24:20 -0400

    Update and rename extensions/dpm/colors.sh to extensions/dpm/formatting/colors.sh

---
+++ extensions/dpm/formatting/colors.sh
@@ -0,0 +1,17 @@
+export color_default="\e[39m"
+export color_black="\e[30m"
+export color_red="\e[31m"
+export color_green="\e[32m"
+export color_yellow="\e[33m"
+export color_blue="\e[33m"
+export color_magenta
+export color_cyan
+export color_lightgray
+export color_darkgray
+export color_lightred
+export color_lightgreen
+export color_lightyellow
+export color_lightblue
+export color_lightmagenta
+export color_lightcyan
+export color_white
