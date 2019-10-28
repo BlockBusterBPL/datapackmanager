eba55cb6bacda3935493e3ffe34e9a6e532ab47d
BlockBusterBPL <beckettloose@me.com>
2019-10-28 11:48:03 -0400

    Create init.sh

---
+++ extensions/dpm/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash  
+dpm_init() {
+  DPM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+  rm -rf "${DPM_DIR}/temp/"
+  mkfifo "${DPM_DIR}/temp/dpm-form"
+  dpm_form="${DPM_DIR}/temp/dpm-form"
+}
