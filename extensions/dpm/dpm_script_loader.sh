f73ed8db9e7df6976c8684ea9764a4f6f60d6274
BlockBusterBPL <beckettloose@me.com>
2020-01-17 09:25:27 -0500

    Update and rename init.sh to dpm_script_loader.sh

--- extensions/dpm/dpm_script_loader.sh
+++ extensions/dpm/dpm_script_loader.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-dpm_init() {
+dpm_script_loader() {
   DPM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
   rm -rf "${DPM_DIR}/temp/"
   mkfifo "${DPM_DIR}/temp/dpm-form"
