c2edf5c511f062a29984c29631f0d836f204c689
BlockBusterBPL <beckettloose@me.com>
2020-01-17 09:25:51 -0500

    Delete dpm_script_loader.sh

--- extensions/dpm/dpm_script_loader.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash  
-dpm_script_loader() {
-  DPM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-  rm -rf "${DPM_DIR}/temp/"
-  mkfifo "${DPM_DIR}/temp/dpm-form"
-  dpm_form="${DPM_DIR}/temp/dpm-form"
-  git submodule update --init --recursive
-}
