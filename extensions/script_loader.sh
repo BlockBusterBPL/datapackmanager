07f1c3d0ef519c7c94e5689a368be126fb170fd3
BlockBusterBPL <beckettloose@me.com>
2020-01-30 14:26:16 -0500

    Update script_loader.sh

--- extensions/script_loader.sh
+++ extensions/script_loader.sh
@@ -1,6 +1,8 @@
 #!/bin/bash  
 script_loader() {
+  # DEFINE DPM DIRECTORY
   DPM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+  # CREATE TEMP FILES FOR DIALOG
   rm -rf "${DPM_DIR}/temp/"
   mkfifo "${DPM_DIR}/temp/dpm-form"
   dpm_form="${DPM_DIR}/temp/dpm-form"
@@ -8,7 +10,13 @@ script_loader() {
   dpm_temp="${DPM_DIR}/temp/dpm-temp"
   mkfifo "${DPM_DIR}/temp/dpm-pkg-temp"
   dpm_pkg_temp="${DPM_DIR}/temp/dpm-pkg-temp"
+  # DEFINE DIALOG HEIGHT AND WIDH
+  dpm_script_loader_width=$(tput cols)
+  dpm_script_loader_height=$(tput lines)
+  h=$(($dpm_script_loader_width / 2))
+  w=$(($dpm_script_loader_height / 2))
+  # DEFINE LOAD ORDER
   for f in ${DPM_DIR}/extensions/libraries/*; do source $f; done
   for g in ${DPM_DIR}/extensions/dpm-source/*; do source $g; done
-  for h in ${DPM_DIR}/extensions/overrides/*; do source $h; done
+  for h in ${DPM_DIR}/extensions/addons/*; do source $h; done
 }
